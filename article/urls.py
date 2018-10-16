2f598a1c425b1eebc845661712734121167b7515
sx-hyy <sx_hyy@163.com>
2018-10-16 14:10:49 +0800

    。

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-
     path('hotArticle/', views.hotArticle, name='hotArticle'),  # 得到热门文章
     url(r'getArticle/(?P<id>\d+)', views.getArticle, name='getArticle'),  # 根据ID得到文章信息
 ]
