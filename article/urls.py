035c4cea7780d17edcbd6564ae1c97dc83e4f7e9
hcbin <1742552339@qq.com>
2018-10-21 11:04:08 +0800

    hcb 2018-10-21 11:00

--- article/urls.py
+++ article/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^getUserArticle/(?P<id>\d+)', views.getUserArticle, name='getUserArticle'),
     url(r'^getCollectArticle/(?P<tel>\d+)', views.getCollectArticle, name='getCollectArticle'),  # 根据tel获取已经收藏的文章
     url(r'^getMyArticle/(?P<tel>\d+)', views.getMyArticle, name='getMyArticle'),  # 根据tel获取我的文章
-    url(r'getComment/(?P<artid>\d+)', views.getComment, name='getComment') # 通过文章ID获取文章所有评论
+    url(r'getComment/(?P<artid>\d+)', views.getComment, name='getComment'), # 通过文章ID获取文章所有评论
     url(r'deleteArticle/(?P<id>\w*)', views.deleteArticle, name='deleteArticle'),  # 删除收藏文章
     url(r'deleteUserArticle/(?P<id>\w*)', views.deleteUserArticle, name='deleteUserArticle'),  # 删除个人文章
 
